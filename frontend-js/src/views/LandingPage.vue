3d9b748a05c115c9272e8bf98138b13d8969bc9a
Avei20 <muhammadabdulazizalghofari@gmail.com>
2022-11-18 22:39:23 +0700

    Update Vue

---
+++ frontend-js/src/views/LandingPage.vue
@@ -0,0 +1,12 @@
+<template>
+    <Sidebarv2/>
+    <div class="m-16">
+        Landing is Here
+    </div>
+</template>
+
+<script>
+import Sidebarv2 from '@/components/Sidebarv2.vue';
+
+    export default{ components: { Sidebarv2 } }
+</script>
